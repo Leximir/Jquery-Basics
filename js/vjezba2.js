a33bdb745088444b83ed8289792ea3dcfe6f0236
Leximir <aleksa.stankovic@proton.me>
2025-07-03 15:06:00 +0200

    Homework complete

--- js/vjezba2.js
+++ js/vjezba2.js
@@ -9,4 +9,30 @@ $(document).ready(function(){
         $("#result").text(result);
     });
 
+    $('#subtract').click(function () {
+
+        let result = Number($('#number').val()) - Number($('#number1').val());
+
+        $("#result").text(result);
+    });
+
+    $('#multiply').click(function () {
+
+        let result = Number($('#number').val()) * Number($('#number1').val());
+
+        $("#result").text(result);
+    });
+
+    $('#divide').click(function () {
+
+        if(Number($('#number1').val()) === 0){
+            $("#result").text("Ne moze se dijeliti nulom");
+            return;
+        }
+
+        let result = Number($('#number').val()) / Number($('#number1').val());
+
+        $("#result").text(result);
+    });
+
 });
